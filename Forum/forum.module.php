033de2ba2630828682421e2746bd82f1383d9b1e
DaVaR <davar@thedavar.net>
2016-06-07 19:16:00 -0500

    Add Version Checker for UAP Admin Panel

--- Forum/forum.module.php
+++ Forum/forum.module.php
@@ -1,13 +1,16 @@
 <?php
-/**  
+/**
 * UserApplePie v3 Forum Plugin
 * @author David (DaVaR) Sargent
 * @email davar@thedavar.net
 * @website http://www.userapplepie.com
-* @version 1.0.0
+* @version 1.0.1
 * @release_date 04/27/2016
 **/
 
 use Helpers\Hooks;
 
 Hooks::addhook('routes', 'App\\Modules\\Forum\\Controllers\\Forum@routes');
+
+/** Define Current Version of UAP **/
+define('UAPForumVersion', 'v1.0.1');
